28431e4ce86e1f8fd3cdef8e368db7a3da725355
jorge.jimenez <jorge.jimenez@dinamicarea.com>
2016-02-08 18:57:50 +0100

    feat: terminado lectura

--- src/test/java/es/training/input/reader/ReaderTest.java
+++ src/test/java/es/training/input/reader/ReaderTest.java
@@ -1,11 +1,24 @@
 package es.training.input.reader;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 import org.junit.Test;
 
 public class ReaderTest {
 
 	@Test
-	public void readLogo() {
+	public void readLogo() throws IOException, URISyntaxException {
+
+		new Reader().read(Paths.get(ReaderTest.class.getResource("/logo.in").toURI()));
+
+	}
+
+	@Test
+	public void readlearn_and_teach() throws IOException, URISyntaxException {
+
+		new Reader().read(Paths.get(ReaderTest.class.getResource("/learn_and_teach.in").toURI()));
 
 	}
 }
